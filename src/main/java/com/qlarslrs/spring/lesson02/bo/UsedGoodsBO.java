60581debe2c0cfd11fe2a3bde3ddbfd77961d38a
JooYong <qlarslrs@naver.com>
2021-07-22 11:55:34 +0900

    [이주용] - myBatis 연동 예제 최초 파일 추가

---
+++ src/main/java/com/qlarslrs/spring/lesson02/bo/UsedGoodsBO.java
@@ -0,0 +1,20 @@
+package com.qlarslrs.spring.lesson02.bo;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.qlarslrs.spring.lesson02.dao.UsedGoodsDAO;
+import com.qlarslrs.spring.lesson02.model.UsedGoods;
+
+// 비지니스 로직
+@Service
+public class UsedGoodsBO {
+
+	@Autowired
+	private UsedGoodsDAO usedGoodsDAO;
+	public List<UsedGoods> getUsedGoodsList() {
+		return usedGoodsDAO.selectUsedGoodsList();
+	}
+}
